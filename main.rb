1d5f96c9d66e528be1c7a70969c33cf6afb4d0ae
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-11-17 16:21:36 +0300

    Accept a character and output corresponding morse code

---
+++ main.rb
@@ -0,0 +1,39 @@
+MORSE_CODE = {
+  A: '.-',
+  B: '-...',
+  C: '-.-.',
+  D: '-..',
+  E: '.',
+  F: '..-.',
+  G: '--.',
+  H: '....',
+  I: '..',
+  J: '.---',
+  K: '-.-',
+  L: '.-..',
+  M: '--',
+  N: '-.',
+  O: '---',
+  P: '.--.',
+  Q: '--.-',
+  R: '.-.',
+  S: '...',
+  T: '-',
+  U: '..-',
+  V: '...-',
+  W: '.--',
+  X: '-..-',
+  Y: '-.--',
+  Z: '--..'
+}.freeze
+
+puts 'Please enter a value'
+values = gets.chomp
+
+def decode_char(char)
+  MORSE_CODE.each do |key, value|
+    puts key.to_s if char == value
+  end
+end
+
+decode_char(values)
